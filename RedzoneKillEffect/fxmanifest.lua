e93ebdd4e37f937013a1e339a5b34f925d266bad
HXVRMXN-dev <107282158+HXVRMXN-dev@users.noreply.github.com>
2023-08-04 18:32:41 +0100

    Add files via upload

---
+++ RedzoneKillEffect/fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'HXVRMXN.dev'
+description 'Redzone Kill Effect #1 - Red'
+version '1.0.0'
+
+file 'timecycle_mods_4.xml'
+
+data_file 'TIMECYCLEMOD_FILE' 'timecycle_mods_4.xml'
